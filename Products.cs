de9b756d6135102c64c7500d441bedd653c583f3
NazarbekDeveloper <bekzalatoy05@gmail.com>
2026-02-21 00:32:48 +0500

    Add project files.

---
+++ Products.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DapperWithOOP
+{
+    public static class Products
+    {
+        public static async Task GetById(int productId)
+        {
+            string? connectionString =
+                                      "Server=BEKZALATOY\\MSSQLSERVER01;" +
+                                      "Database=NorthWindDb;" +
+                                      "Integrated Security=True;" +
+                                      "TrustServerCertificate=True;";
+            IDbConnection ulanish = new SqlConnection(connectionString);
+
+            string sqlQuery =
+                """
+                select * from Products
+                where ProductID = @productId;
+                """;
+
+            Product? product = await ulanish.QueryFirstOrDefaultAsync<Product>(sqlQuery, new { productId });
+
+            Console.WriteLine($"ProductId: {product.ProductId} ");
+            Console.WriteLine($"ReorderLevel: {product.ReorderLevel}");
+            Console.WriteLine($"Discontinued: {product.Discontinued}");
+            Console.WriteLine($"ProductName: {product.ProductName} ");
+            Console.WriteLine($"SupplierId: {product.SupplierId}");
+            Console.WriteLine($"CategoryId: {product.CategoryId}");
+            Console.WriteLine($"QuantityPerUnit: {product.QuantityPerUnit}");
+            Console.WriteLine($"UnitPrice: {product.UnitPrice}");
+            Console.WriteLine($"UnitsInStock: {product.UnitsInStock}");
+            Console.WriteLine($"UnitsOnOrder: {product.UnitsOnOrder}");
+            Console.WriteLine();
+        }
+        public static async Task GetAll()
+        {
+            string? connectionString =
+                                  "Server=BEKZALATOY\\MSSQLSERVER01;" +
+                                  "Database=NorthWindDb;" +
+                                  "Integrated Security=True;" +
+                                  "TrustServerCertificate=True;";
+            IDbConnection ulanish = new SqlConnection(connectionString);
+            string sqlQuery =
+                    """
+                    select * from Products;
+                    """;
+            IEnumerable<Product> products = await ulanish.QueryAsync<Product>(sqlQuery);
+            foreach (var product in products)
+            {
+                Console.WriteLine($"ProductId: {product.ProductId} ");
+                Console.WriteLine($"ReorderLevel: {product.ReorderLevel}");
+                Console.WriteLine($"Discontinued: {product.Discontinued}");
+                Console.WriteLine($"ProductName: {product.ProductName} ");
+                Console.WriteLine($"SupplierId: {product.SupplierId}");
+                Console.WriteLine($"CategoryId: {product.CategoryId}");
+                Console.WriteLine($"QuantityPerUnit: {product.QuantityPerUnit}");
+                Console.WriteLine($"UnitPrice: {product.UnitPrice}");
+                Console.WriteLine($"UnitsInStock: {product.UnitsInStock}");
+                Console.WriteLine($"UnitsOnOrder: {product.UnitsOnOrder}");
+                Console.WriteLine();
+            }
+        }
+        public static async Task SearchByName(string name)
+        {
+            string? connectionString =
+                                 "Server=BEKZALATOY\\MSSQLSERVER01;" +
+                                 "Database=NorthWindDb;" +
+                                 "Integrated Security=True;" +
+                                 "TrustServerCertificate=True;";
+            IDbConnection ulanish = new SqlConnection(connectionString);
+            string sqlQuery =
+                    """
+                    select ProductName from Products
+                    where ProductName LIKE '%@name%';
+                    """;
+            IEnumerable<string> productNames = await ulanish.QueryAsync<string>(sqlQuery, new { name });
+            foreach(string productName in productNames)
+            {
+                Console.WriteLine($"ProductName: {productName}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using DapperWithOOP;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace DapperWithOOP
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Qaysi table bilan ishlaymiz\n1.Products\n2.Customers\n3.Orders");
+            Console.Write(">>> ");
+            int tableNumber = int.Parse(Console.ReadLine());
+            switch (tableNumber)
+            {
+                case 1:
+                    Console.WriteLine("Qanday vazifa bajaramiz?\n1.GetByID\n2.GetAll\n3.SearchByName");
+                    Console.Write(">>> ");
+                    int vazifa = int.Parse(Console.ReadLine());
+                    switch (vazifa)
+                    {
+                        case 1:
+                            Console.Write("IDni kiriting: ");
+                            int productId = Convert.ToInt32(Console.ReadLine());
+                            _=Products.GetById(productId);
+                            break;
+                        case 2:
+                            _=Products.GetAll();
+                            break;
+                        case 3:
+                            Console.WriteLine("Name ning biror qismini kiriting ");
+                            Console.Write(">>> ");
+                            string name = Console.ReadLine();
+                            _ = Products.SearchByName(name);
+                            break;
+
+                    }
+                    break;
+
+
+            }
+
+        }
+
+    }
+}
